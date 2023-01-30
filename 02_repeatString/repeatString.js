a4222a7d111d9745e126da13a21f1496abeae9f3
Ameera <ameera.gahussain@gmail.com>
2023-01-30 21:56:55 +0800

    Add loop to function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
-
+const repeatString = function(word, count) {
+    if (count < 0) return 'ERROR';
+    let repeatedString = '';
+    for (let i = 0; i < count; i++) {
+        repeatedString += word;
+    }
+    return repeatedString;
 };
+
+// Solution
+// const repeatString = function(word, times) {
+//    if (times < 0) return 'ERROR';
+//    let string = '';
+//    for (let i = 0; i < times; i++) {
+//        string += word;
+//   }
+//    return string;
+// };
 
 // Do not edit below this line
 module.exports = repeatString;
